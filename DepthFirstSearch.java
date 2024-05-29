f044ea524f9ce673930f904f740d1f6247284d60
dvn1vl <154106380+dvn1vl@users.noreply.github.com>
2024-05-30 02:49:04 +0500

    Implementation of DepthFirstSearch and BreadthFirstSearch classes

---
+++ DepthFirstSearch.java
@@ -0,0 +1,16 @@
+public class DepthFirstSearch<Vertex> extends Search<Vertex> {
+    public DepthFirstSearch(WeightedGraph<Vertex> graph, Vertex source) {
+        super(source);
+        dfs(graph, source);
+    }
+
+    private void dfs(WeightedGraph<Vertex> graph, Vertex current) {
+        marked.add(current);
+        for (Vertex v : graph.getNeighbours(current)) {
+            if (!marked.contains(v)) {
+                edgeTo.put(v, current);
+                dfs(graph, v);
+            }
+        }
+    }
+}
