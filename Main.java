8ca8f0ef78903bd6b96bb75df89f2ed8eec045d7
dvn1vl <154106380+dvn1vl@users.noreply.github.com>
2024-05-30 07:33:29 +0500

    Checking final variation in Main

---
+++ Main.java
@@ -0,0 +1,45 @@
+
+public class Main{
+    public static void main(String[] args) {
+        WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
+        fillGraph(weightedGraph);
+
+        System.out.println("Dijkstra:");
+        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
+        outputPath(djk, "Kyzylorda");
+
+
+        System.out.println("--------------------------------");
+
+        WeightedGraph<String> graph = new WeightedGraph<>(true);
+        fillGraph(graph);
+
+        System.out.println("DFS:");
+        Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
+        outputPath(dfs, "Kyzylorda");
+
+        System.out.println("--------------------------------");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
+        outputPath(bfs, "Kyzylorda");
+    }
+
+    public static void fillGraph(WeightedGraph<String> graph) {
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Shymkent", "Atyrau", 7.8);
+        graph.addEdge("Atyrau", "Astana", 7.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+    }
+
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+
+        System.out.println();
+    }
+}
